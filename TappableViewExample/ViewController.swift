7795e650b0c83bd8a3ede64b3a744d43cb3e9e7a
Malinka Seneviratne <malinkas86@gmail.com>
2019-07-24 09:06:42 -0700

    Programmatically creating the tappableLabelView

--- TappableViewExample/ViewController.swift
+++ TappableViewExample/ViewController.swift
@@ -11,8 +11,9 @@ import TappableLabelView
 import Marker
 
 class ViewController: UIViewController {
+    
+    var tappableLabelView: TappableLabelView!
 
-    @IBOutlet weak var tappableLabelView: TappableLabelView!
     private let textStyle = TextStyle(font: UIFont.systemFont(ofSize: 12),
                                    textColor: UIColor(red: 0.2, green: 0.2, blue: 0.22, alpha: 1),
                                    characterSpacing: nil,
@@ -40,13 +41,25 @@ class ViewController: UIViewController {
                                       textTransform: .none)
     override func viewDidLoad() {
         super.viewDidLoad()
-        tappableLabelView.options = ConfigurationOptions(textAttributes: textStyle.attributes,
-                                                         highlightedTextAttributes: highlightedTextStyle.attributes,
-                                                         alignment: .center,
-                                                         isUnderline: true,
-                                                         delegate: self)
-        tappableLabelView.text = "I have a UICollectionViewCell drawn out in a xib and accompanied by swift class file. These two files are part of my app's UI framework target (called SomeUI). In the view controller of my collection view (in the app target) I register the nib as a collection view cell. My view controller code is as follows hello I am young UICollectionViewCell"
+        let options = ConfigurationOptions(textAttributes: textStyle.attributes,
+                                           highlightedTextAttributes: highlightedTextStyle.attributes,
+                                           alignment: .center,
+                                           isUnderline: true,
+                                           wordSpacing: 5,
+                                           lineSpacing: 6,
+                                           delegate: self)
+        tappableLabelView = TappableLabelView(frame: .zero, options: options)
+        tappableLabelView.translatesAutoresizingMaskIntoConstraints = false
+        tappableLabelView.text = "I have a UICollectionViewCell drawn out in a xib and accompanied by swift class file. These two files are part of my app's UI framework target (called SomeUI). In the view controller of my collection view (in the app target) I register the nib as a collection view cell. My view controller code is as follows hI have a UICollectionViewCell drawn out in a xib"
         tappableLabelView.tappableStrings = ["collection", "register", "cell.", "UICollectionViewCell"]
+        view.addSubview(tappableLabelView)
+        NSLayoutConstraint.activate([
+                tappableLabelView.heightAnchor.constraint(equalToConstant: 200),
+                tappableLabelView.widthAnchor.constraint(equalTo: view.widthAnchor),
+                tappableLabelView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                tappableLabelView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                tappableLabelView.topAnchor.constraint(equalTo: view.topAnchor)
+            ])
     }
 
 }
@@ -54,8 +67,10 @@ class ViewController: UIViewController {
 // MARK: - TappableLabelViewDelegate
 extension ViewController: TappableLabelViewDelegate {
     
-    func didTap(text: String, indexInText: Int, index: Int) {
-        print("text: \(text) indexInText: \(indexInText) index: \(index)")
+    func didTap(tappableLabelView: TappableLabelView, text: String, indexInText: Int, index: Int) {
+        if self.tappableLabelView == tappableLabelView {
+            print("text: \(text) indexInText: \(indexInText) index: \(index)")
+        }
     }
 
 }
